09c161af03440fe8a4a0346de8e86a2ee27ad9aa
Rhys Adams <email.curly@gmail.com>
2019-03-10 17:30:22 +0000

    Updated to gradle - for compatibility with my mobile version.

---
+++ src/main/com/RhysChat/EmojiFormatter.java
@@ -0,0 +1,23 @@
+package com.RhysChat;
+
+import emoji4j.EmojiUtils;
+
+class EmojiFormatter {
+
+    String toEmoji(String input){
+        try {
+            String formatted = EmojiUtils.emojify(input);
+            return formatted;
+        }catch(NullPointerException n){
+            return input;
+        }
+    }
+    String toPlainText(String input) {
+        try {
+            String formatted = EmojiUtils.shortCodify(input);
+            return formatted;
+        } catch (NullPointerException n) {
+            return input;
+        }
+    }
+}
